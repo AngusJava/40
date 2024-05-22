d205dedf8cf0f27343d2a9cbc6c6bbfc72f32a78
AngusJava <161606097+AngusJava@users.noreply.github.com>
2024-05-22 12:11:04 +0000

    4_5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,6 +30,9 @@ class Main {
             Scanner wiek = new Scanner(System.in);
             Scanner nazwisko = new Scanner(System.in);
             Scanner data_dnia = new Scanner(System.in);
+            Scanner data_dzien = new Scanner(System.in);
+            Scanner data_miesiac = new Scanner(System.in);
+            Scanner data_rok = new Scanner(System.in);
             System.out.print("Podaj imie: ");
             String name = imie.nextLine();
             System.out.print("Podaj nazwisko: ");
@@ -38,7 +41,25 @@ class Main {
             int age = wiek.nextInt();
             System.out.print("Podaj Datę dnia:  ");
             String date = data_dnia.nextLine();
-            s.addStudent(new Student(name, lname, age, date));
+            System.out.print("Podaj dzien:  ");
+            int date_day = data_dnia.nextInt();
+            System.out.print("Podaj miesiac  ");
+            int date_mouth = data_dnia.nextInt();
+            System.out.print("Podaj rok ");
+            int date_year = data_dnia.nextInt();
+            if (date_day > 31) {
+              System.out.print("dzien spoza zakresu");
+              break;
+            }
+            if (date_mouth > 12) {
+              System.out.print("miesiac spoza zakresu");
+              break;
+            }
+            if (date_year < 1900 || date_year > 2025) {
+              System.out.print("rok nie w zakresie");
+              break;
+            }
+            s.addStudent(new Student(name, lname, age, date, date_day, date_mouth, date_year));
             break;
           case 2:
             var students = s.getStudents();
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -4,14 +4,18 @@ public class Student {
   private int Age;
   private String Lname;
   private String Date;
+  private int Date_day;
+  private int Date_mouth;
+  private int Date_year;
 
-  public Student(String name, String lname, int age, String date) {
+  public Student(String name, String lname, int age, String date, int data_day, int data_mouth, int data_year) {
     Name = name;
     Age = age;
-    // 4_2
     Lname = lname;
-    // 4_4 date urodzenia w innej formie
     Date = date;
+    Date_day = data_day;
+    Date_mouth = data_mouth;
+    Date_year = data_year;
   }
 
   public String GetName() {
@@ -23,13 +27,15 @@ public class Student {
   }
 
   public String ToString() {
-    return Name + " " + Lname + " " + Integer.toString(Age) + " " + Date;
+    return Name + " " + Lname + " " + Integer.toString(Age) + " " + Date + " " + Date_day + "-" + Date_mouth + "-"
+        + Date_year;
   }
 
   public static Student Parse(String str) {
-    String[] data = str.split(" ");
-    if (data.length != 4)
-      return new Student("Parse Error", "error", -1, "error");
-    return new Student(data[0], data[1], Integer.parseInt(data[2]), data[3]);
+    String[] data = str.split("[ -]");
+    if (data.length != 7)
+      return new Student("Parse Error", "error", -1, "error", -1, -1, -1);
+    return new Student(data[0], data[1], Integer.parseInt(data[2]), data[3], Integer.parseInt(data[4]),
+        Integer.parseInt(data[5]), Integer.parseInt(data[6]));
   }
 }
